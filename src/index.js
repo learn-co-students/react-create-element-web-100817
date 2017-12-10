5d8180eddc28bbce64cd8846ea8dd545f72cafe0
evaccaro <erv230@nyu.edu>
2017-12-09 20:19:14 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-// Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const list =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'My favorite ice cream flavors'),
+    React.createElement('ul', {},
+      [
+        React.createElement('li', { className: 'brown', key: 1}, 'Chocolate'),
+        React.createElement('li', { className: 'white', key: 2}, 'Vanilla'),
+        React.createElement('li', { className: 'yellow', key: 3}, 'Banana')
+      ]
+    )
+  );
+
+ReactDOM.render(
+  list,
+  document.getElementById('global')
+);
